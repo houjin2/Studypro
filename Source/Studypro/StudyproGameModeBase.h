c0c0f8cd8e1e73086aa6dbc7ff153bce1906c257
houjin2 <dlaghwls456@gmail.com>
2024-02-16 23:03:53 +0900

    feat : 프로젝트 생성

---
+++ Source/Studypro/StudyproGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StudyproGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDYPRO_API AStudyproGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
